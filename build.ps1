bf4401891f4b588f35f7ff80204fe97a7dd2d195
upperdim <45048352+upperdim@users.noreply.github.com>
2024-08-09 23:59:51 +0200

    added compiler flag

--- build.ps1
+++ build.ps1
@@ -1,4 +1,4 @@
 cd src
-gcc -c *.c -Wall -I"C:\libsdl\include" -L"C:\libsdl\lib" -lmingw32 -lSDL2main -lSDL2
+gcc -c *.c -Wall -Wextra -I"C:\libsdl\include" -L"C:\libsdl\lib" -lmingw32 -lSDL2main -lSDL2
 gcc -s *.o -I"C:\libsdl\include" -L"C:\libsdl\lib" -lmingw32 -lSDL2main -lSDL2 -o .\..\raycaster.exe
 cd ..
