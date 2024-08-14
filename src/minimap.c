137b6947ddb19f7931e224c7bf2af0b6122d4296
upperdim <45048352+upperdim@users.noreply.github.com>
2024-08-14 02:44:38 +0200

    dont draw lines outside of minimap

--- src/minimap.c
+++ src/minimap.c
@@ -1,12 +1,31 @@
+#include <stdio.h>
 #include <math.h>
 #include "minimap.h"
 
-static void	draw_line_from_player(Screen *screen, Player *player, float scaleHoriz, float scaleVert, float angleOffset)
+// Lot of ugly parameters but it gives an accurate representation of FOV in minimap
+static void	draw_line_from_player(Screen *screen,
+                                  Map *map,
+                                  Player *player,
+								  float scaleHoriz,
+								  float scaleVert,
+								  float angleOffset,
+								  float lineLen,
+								  int thickness,
+								  Color lineColor)
 {
-	for (float frontDist = 4; frontDist > 0; frontDist -= .05) {
+	for (float frontDist = lineLen; frontDist > 0; frontDist -= .05) {
 		float frontPointX = player->posx + frontDist * sin(player->angle + angleOffset);
 		float frontPointY = player->posy + frontDist * cos(player->angle + angleOffset);
-		draw_rect(screen, frontPointX * scaleHoriz, frontPointY * scaleVert, 2, 2, (Color) {175, 50, 0, 255});
+
+		if (frontPointX < map->width && frontPointY < map->height) {
+			draw_rect(
+				screen,
+				(frontPointX * scaleHoriz) - thickness / 2,
+				(frontPointY * scaleVert) - thickness / 2,
+				thickness,
+				thickness,
+				lineColor);
+		}
 	}
 }
 
@@ -21,13 +40,7 @@ void draw_minimap(Screen *screen, Player *player, Map *map, Npc *npcs)
 	float scaleVertical   = (screen->height / 3) / map->height;
 
 	// Draw background of minimap
-	draw_rect(
-		screen,
-		0,
-		0,
-		scaleHorizontal * map->width,
-		scaleVertical * map->height,
-		(Color) {50, 50, 50, MINIMAP_TRANSPARENCY_ALPHA});
+	draw_rect( screen, 0, 0, scaleHorizontal * map->width, scaleVertical * map->height, (Color) {50, 50, 50, MINIMAP_TRANSPARENCY_ALPHA});
 
 	// Draw map tiles
 	int tile_width  = 1 * scaleHorizontal;
@@ -49,12 +62,17 @@ void draw_minimap(Screen *screen, Player *player, Map *map, Npc *npcs)
 	draw_rect(screen, player->posx * scaleHorizontal - 1, player->posy * scaleVertical - 1, 3, 3, PLAYER_COLOR);
 
 	// Draw player direction
-	draw_line_from_player(screen, player, scaleHorizontal, scaleVertical, 0);
+	// draw_line_from_player(screen, player, scaleHorizontal, scaleVertical, 0, 1, 1, (Color) {150, 50, 50, MINIMAP_TRANSPARENCY_ALPHA});
 
 	// Draw player rays
-	draw_line_from_player(screen, player, scaleHorizontal, scaleVertical, +fov / 2);
-	draw_line_from_player(screen, player, scaleHorizontal, scaleVertical, -fov / 2);
+	const Color FOV_LINE_COLOR = (Color) {50, 50, 125, MINIMAP_TRANSPARENCY_ALPHA};
+	const float FOV_LINE_LEN = 16;
+	const int FOV_LINE_THICKNESS = 1;
+
+	draw_line_from_player(screen, map, player, scaleHorizontal, scaleVertical, +fov / 2, FOV_LINE_LEN, FOV_LINE_THICKNESS, FOV_LINE_COLOR);
+	draw_line_from_player(screen, map, player, scaleHorizontal, scaleVertical, -fov / 2, FOV_LINE_LEN, FOV_LINE_THICKNESS, FOV_LINE_COLOR);
 	
+	// Draw all rays
 	// float raycast_angle_step = fov / screen->width;
 	// for (float angle_offset = -fov / 2; angle_offset <= fov / 2; angle_offset += raycast_angle_step) {
 	// 	draw_line_from_player(screen, player, scaleHorizontal, scaleVertical, angle_offset);
